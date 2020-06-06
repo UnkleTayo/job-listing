2f5c0a131ddb711bfd8f84983d2132587fef72f2
Adetayo <akinsanyaadetayo7@gmail.com>
2020-06-06 18:18:10 +0100

    fixed desktop layout

---
+++ script.js
@@ -0,0 +1,56 @@
+const itemContainer = document.getElementById('container__cards');
+
+getData();
+
+async function getData() {
+  const res = await fetch('/data.json');
+  const data = await res.json();
+
+  displayData(data);
+}
+
+const displayData = (data) => {
+  itemContainer.innerHTML = '';
+  function test(item, msg) {
+    if (item == 'true') {
+      console.log(item);
+      return msg;
+    }
+  }
+
+  data.forEach((data) => {
+    const itemEl = document.createElement('div');
+    itemEl.classList.add('container__cards--item');
+
+    itemEl.innerHTML = `
+    <div class="item-child" id="${data.id}">
+    <div class="img-container">
+      <img src="${data.logo}" alt="${data.company}" class="image">
+    </div>
+    <div>
+      <div class='skills'>
+        <span>${data.company}</span>
+        <span class='${test(data.new, 'New')}'></span>
+        <span class="features">Features</span>
+      </div>
+      <h4>${data.position}</h4>
+      <div class='timeline'>
+        <p class="posted">
+          ${data.postedAt}
+        </p>
+        <p>${data.contract}</p>
+        <p>${data.location}</p>
+      </div>
+    </div>
+    <div></div>
+  </div>
+
+  <div class='slack'>
+    <p>${data.role}</p>
+    <p>${data.level}</p>
+    <p>${[...data.languages].map((data) => data)}</p>
+  </div>`;
+
+    // itemContainer.appendChild(itemEl);
+  });
+};
